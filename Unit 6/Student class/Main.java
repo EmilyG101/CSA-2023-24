f7e86f33df96e865cc8b6b29c65d5cf613fb77c0
EmilyG101 <101006556+EmilyG101@users.noreply.github.com>
2024-06-26 01:14:27 -0400

    Create Main.java

---
+++ Unit 6/Student class/Main.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    //int[] array = {23,45,64,76,88};
+    Student s1 = new Student("Ebby", 230973372, 11, new int[] {23,45,64,76,8});
+    s1.userInfo();
+  }
+}
